7b39f3c5c74da8c1864aa6b772ce979b92e6aeea
Alexander Popov <alex.wayfer@gmail.com>
2020-04-09 00:36:30 +0300

    Add `rubocop-performance` and `rubocop-rspec`
    Resolve new offenses.

--- spec/flame/cli/new_spec.rb
+++ spec/flame/cli/new_spec.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
-describe 'FlameCLI::New' do
+describe 'Flame::CLI::New' do
 	describe '--help' do
 		subject { `#{FLAME_CLI} new --help` }
 
-		it do
-			is_expected.to match_words(
+		let(:expected_words) do
+			[
 				'Usage:', 'flame new [OPTIONS] SUBCOMMAND [ARG]',
 				'Subcommands:', 'application, app',
 				'Options:', '-h, --help'
-			)
+			]
 		end
+
+		it { is_expected.to match_words(*expected_words) }
 	end
 end
--- spec/flame/cli_spec.rb
+++ spec/flame/cli_spec.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
-describe 'FlameCLI::Flame' do
+describe 'Flame::CLI' do
 	describe '--help' do
 		subject { `#{FLAME_CLI} --help` }
 
-		it do
-			is_expected.to match_words(
+		let(:expected_words) do
+			[
 				'Usage:', 'flame [OPTIONS] SUBCOMMAND [ARG]',
 				'Subcommands:', 'initialize, init, new',
 				'Options:', '-h, --help'
-			)
+			]
 		end
+
+		it { is_expected.to match_words(*expected_words) }
 	end
 end
